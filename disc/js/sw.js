960029acad65e88673c07bfd61541225e77116b5
brunocriacoes <br.rafael@outlook.com>
2018-07-30 17:51:37 -0300

    ajuste sw

--- disc/js/sw.js
+++ disc/js/sw.js
@@ -1,20 +1,29 @@
 const CACHE_NAME  = "brc-pwa";
 const CACHE_FILES = [
-    './',
-    './disc/css/mobile.css',
-    './index.html'
+    '/',
+    '/disc/css/mobile.css',
+    '/disc/logo/ico.png'
 ];
 
-self.addEventListener( "install", function( evento ) {
-    evento.waitUntil(
-        caches.open( CACHE_NAME )
-        .then( cache => cache.addAll( CACHE_FILES ))
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(CACHE_FILES);
+        })
     );
-} );
+  });
 
-self.addEventListener( "fetch", function( evento ) {
-    evento.waitUntil(
-        caches.open( CACHE_NAME )
-        .then( cache => cache.addAll( CACHE_FILES ))
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+            return response;
+            }
+            return fetch(event.request);
+        }
+        )
     );
-} );
+});
