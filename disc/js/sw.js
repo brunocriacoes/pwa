aa493989ad19b44b19a5e806415142c85400d64a
brunocriacoes <br.rafael@outlook.com>
2018-07-30 18:51:11 -0300

    ajuste pwa by google

--- disc/js/sw.js
+++ disc/js/sw.js
@@ -1,29 +1,58 @@
-const CACHE_NAME  = "brc-pwa";
-const CACHE_FILES = [
-    './',
-    './disc/css/mobile.css',
-    './disc/logo/ico.png'
+
+var dataCacheName = 'pwa-brc';
+var cacheName = 'pwa-brc-final';
+var filesToCache = [
+    '/',
+    '/disc/css/style.css',
+    '/disc/css/mobile.css',
+    '/disc/js/app.js',
+    '/disc/js/config.js',
+    '/disc/js/help.js', 
+    '/disc/logo/logo.svg'   
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(CACHE_FILES);
-        })
-    );
-  });
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            if (response) {
-            return response;
-            }
-            return fetch(event.request);
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
         }
-        )
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
     );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
