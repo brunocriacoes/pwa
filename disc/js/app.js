915c351f6d270d5111f5f0593ee6584d1ba53bfe
brunocriacoes <br.rafael@outlook.com>
2018-08-01 17:33:13 -0300

    push

--- disc/js/app.js
+++ disc/js/app.js
@@ -2,3 +2,18 @@
     if( window.navigator && window.navigator.vibrate ) {
         window.navigator.vibrate( [100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100] );
     }
+
+    if ( "Notification" in window ) {
+        let ask = Notification.requestPermission();
+        ask.then( permission => {
+            if ( permission === "granted" ) {
+                let msg = new Notification( "SOS", {
+                        body: "Eu sou uma mensagem push",
+                        icon : "./disc/logo/icon-152x152.png"
+                } );
+                msg.addEventListener( "click", event => {
+                    alert( "ação do click" );
+                } );
+            }
+        } );
+    }
